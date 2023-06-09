05a5edbd5e09ced05880079265c3174d65e59f79
EikoFresh <109620716+EikoFresh@users.noreply.github.com>
2023-06-09 21:17:21 +0200

    norminette fix and echo fix

--- minishell/srcs/builtins/unset.c
+++ minishell/srcs/builtins/unset.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estruckm <estruckm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/09 21:12:50 by estruckm          #+#    #+#             */
+/*   Updated: 2023/06/09 21:12:53 by estruckm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/minishell.h"
 
-void ft_unset_list(t_env **list_pointer, char *string)
+void	ft_unset_list(t_env **list_pointer, char *string)
 {
-	t_env *search_node;
-	t_env *previous_node;
+	t_env	*search_node;
+	t_env	*previous_node;
+	t_env	*tmp;
 
 	search_node = ft_get_env_node(*list_pointer, string);
 	if (search_node == NULL)
-		return;
+		return ;
 	if (search_node == *list_pointer)
 	{
-		t_env *tmp;
 		tmp = *list_pointer;
 		*list_pointer = (*list_pointer)->next;
 		ft_free_node(tmp);
@@ -23,13 +35,13 @@ void ft_unset_list(t_env **list_pointer, char *string)
 	}
 }
 
-void ft_unset(t_cmds *cmd, t_mVars *list_pointer)
+void	ft_unset(t_cmds *cmd, t_mVars *list_pointer)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (!cmd->args)
-		return;
+		return ;
 	while (cmd->args[i])
 	{
 		ft_unset_list(&list_pointer->ls_export, cmd->args[i]);
@@ -38,8 +50,3 @@ void ft_unset(t_cmds *cmd, t_mVars *list_pointer)
 		i++;
 	}
 }
-
-
-
-
-
